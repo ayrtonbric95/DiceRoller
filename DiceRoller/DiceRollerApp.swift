42bfe00f3194ba40391e271de9d5548b4f5d978e
Ayrton Briceno <iqbriceno@gmail.com>
2024-06-26 22:34:29 -0400

    Initial Commit

---
+++ DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Ayrton Briceno on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
