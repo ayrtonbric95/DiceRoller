5ea78436d6601c4fb53ec9401a375bef7254a057
Ayrton Briceno <iqbriceno@gmail.com>
2024-07-22 21:54:55 -0400

    Added colors.

---
+++ DiceRoller/DiceView.swift
@@ -0,0 +1,18 @@
+//
+//  DiceView.swift
+//  DiceRoller
+//
+//  Created by Ayrton Briceno on 6/26/24.
+//
+
+import SwiftUI
+
+struct DiceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DiceView()
+}
